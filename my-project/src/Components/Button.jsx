d6276897e407a911346311e6da15fa7fd70319a0
M-Aaun-two <aaunmalik@outlook.com>
2024-08-22 18:58:27 +0500

    added special offer

--- my-project/src/Components/Button.jsx
+++ my-project/src/Components/Button.jsx
@@ -1,15 +1,22 @@
-const Button = ({ label, iconURL }) => {
-    return (
-      <button className="flex items-center justify-center gap-2 px-7 py-4 border font-montserrat text-lg leading-none bg-coral-red rounded-full text-white border-coral-red hover:scale-110 transform transition duration-300">
-        {label}
-        {iconURL && <img 
+const Button = ({ label, iconURL, backgroundColor, borderColor, textColor }) => {
+  return (
+    <button
+      className={`flex items-center justify-center gap-2 px-7 py-4 border font-montserrat text-lg leading-none 
+        ${backgroundColor || "bg-coral-red"} 
+        ${textColor || "text-white"} 
+        ${borderColor || "border-coral-red"} 
+        rounded-full hover:scale-110 transform transition duration-300`}
+    >
+      {label}
+      {iconURL && (
+        <img 
           src={iconURL} 
           alt="icon" 
           className="ml-2 rounded-full w-5 h-5"
-        />}
-      </button>
-    );
-  }
-  
-  export default Button;
-  
+        />
+      )}
+    </button>
+  );
+}
+
+export default Button;
--- my-project/src/sections/specialOffers.jsx
+++ my-project/src/sections/specialOffers.jsx
@@ -1,8 +1,34 @@
-
+import { arrowRight } from "../assets/icons"
+import { offer } from "../assets/images"
+import Button from "../Components/Button"
 
 const specialOffers = () => {
   return (
-    <div>specialOffers</div>
+    <section className="flex justify-wrap items-center max-xl:flex-col-reverse gap-10 max-container">
+      <div className="flex-1">
+        <img src={offer} alt="offer"
+        width={733} height={687}
+        className="object-contain w-full hover:scale-105 transform transition duration-300"
+        />
+      </div>
+      <div className="flex flex-1 flex-col">
+        <h2 className=" font-palanquin text-4xl capitalize font-bold lg:max-w-lg">
+          <span className="text-coral-red">
+          Special
+          </span> Offer
+        </h2>
+        <p className=" mt-4 lg:max-w-lg info-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Qui quas doloremque consequuntur placeat sunt, ab provident harum, at, dicta ducimus voluptas similique nemo a nihil? Ipsum odit dicta sapiente repellat.</p>
+        <p className="mt-6 lg:max-w-lg info-text">This is just a test web to show case in profile made by Aaun. Not a real nike website</p>
+        <div className="mt-11 flex flex-wrap gap-4">
+          <Button label=" Shoe now" iconURL={arrowRight}/>
+          <Button label="Learn more" 
+          backgroundColor="bg-white"
+          borderColor="border-slate-gray"
+          textColor="text-slate-gray"
+          />
+        </div>
+      </div>
+    </section>
   )
 }
 
