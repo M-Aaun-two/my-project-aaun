63459c7daf33ce8ee846572587df5e2c50687357
M-Aaun-two <aaunmalik@outlook.com>
2024-08-24 18:44:18 +0500

    Customer and subscribe

--- my-project/src/sections/customerReviews.jsx
+++ my-project/src/sections/customerReviews.jsx
@@ -1,8 +1,27 @@
-
+import { reviews } from "../constants"
+import ReviewCard from "../Components/ReviewCard"
 
 const customerReviews = () => {
   return (
-    <div>customerReviews</div>
+    <section className="max-container">
+      <h3 className="font-palanquin text-center text-4xl font-bold">What Our 
+        <span className="text-coral-red"> Customers </span>
+        Say?
+      </h3>
+      <p className="info-text m-auto mt-4 max-w-lg text-center">Lorem ipsum dolor sit amet consectetur adipisicing elit. Maiores harum temporibus at hic, aperiam veritatis.</p>
+
+      <div className="mt-24 flex flex-1 justify-evenly items-center max-lg:flex-col gap-x-14">
+        {reviews.map((review) => (
+          <ReviewCard 
+          key={review.customerName}
+          imgURL={review.imgURL}
+          customerName={review.customerName}
+          rating={review.rating}
+          feedback={review.feedback}
+          />
+        ))}
+      </div>
+    </section>
   )
 }
 
